976a014cc10feba205e46fa7c51003c21a2f1c9b
Emir <40762681+emirgo@users.noreply.github.com>
2020-07-13 16:26:55 +0200

    Task creation and deletion

---
+++ Tasks/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "stm32f10x.h"                  // Device header
+#include "FreeRTOS.h"                   // ARM.FreeRTOS::RTOS:Core
+#include "task.h"                       // ARM.FreeRTOS::RTOS:Core
+
+TaskHandle_t myTask1Handle = NULL;
+
+void myTask1(void *p)
+{
+	int count = (int*) p;
+	
+	while(1)
+	{
+		count++;
+		// 1 sec delay
+		vTaskDelay(1000);
+		
+		// If goes beyond 30 delete the task
+		if (count > 30)
+		{
+			vTaskDelete(myTask1Handle);
+		}
+	}
+}
+
+int main(void)
+{
+	int count = 0;
+	// Pass count
+	xTaskCreate(myTask1, "Task-1", 200, (void*) count, tskIDLE_PRIORITY, &myTask1Handle);
+	// Schedule tasks
+	vTaskStartScheduler();
+	
+	while(1)
+	{
+		
+	}
+	
+	return 0;
+}
