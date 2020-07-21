331e48ac8b05b10cc361edacc249add0ff932785
Emir <40762681+emirgo@users.noreply.github.com>
2020-07-21 19:25:36 +0200

    Task suspension etc

--- Tasks/main.c
+++ Tasks/main.c
@@ -4,18 +4,25 @@
 #include "task.h"                       // ARM.FreeRTOS::RTOS:Core
 
 TaskHandle_t myTask1Handle = NULL;
+TaskHandle_t myTask2Handle = NULL;
 
 void myTask1(void *p)
 {
-	TickType_t myLastUnblock = xTaskGetTickCount();
+	TickType_t myLastUnblock;
 	int count = (int*) p;
 	
 	while(1)
 	{
-		count++;
-		// 1 sec delay
-		vTaskDelayUntil(&myLastUnblock, 1000 * configTICK_RATE_HZ / 1000);
 		
+		// 1 sec delay
+		// vTaskDelay is not as precise as vTaskDelayUntil
+		// I prefer vTaskDelay for my audio projects to give the 
+		// analogue feel.
+		// vTaskDelay(pdMS_TO_TICKS(1000));
+		myLastUnblock = xTaskGetTickCount();
+		vTaskDelayUntil(&myLastUnblock, pdMS_TO_TICKS(1000));
+		
+		count++;
 		// If goes beyond 30 delete the task
 		if (count > 30)
 		{
@@ -24,11 +31,24 @@ void myTask1(void *p)
 	}
 }
 
+void myTask2(void *p)
+{
+	while(1)
+	{
+		vTaskDelay(pdMS_TO_TICKS(5000));
+		vTaskSuspend(myTask1Handle);
+		vTaskDelay(pdMS_TO_TICKS(5000));
+		vTaskResume(myTask1Handle);
+	}
+}
+
 int main(void)
 {
 	int count = 0;
 	// Pass count
 	xTaskCreate(myTask1, "Task-1", 200, (void*) count, tskIDLE_PRIORITY, &myTask1Handle);
+	
+	xTaskCreate(myTask2, "Task-2", 200, (void*) count, tskIDLE_PRIORITY, &myTask2Handle);
 	// Schedule tasks
 	vTaskStartScheduler();
 	
